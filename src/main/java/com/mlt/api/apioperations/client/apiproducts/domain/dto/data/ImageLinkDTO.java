ef1fb33bfee8e37dc4f7782308b75f8d10ab5783
RustyCrazyPunky <crazypunky@protonmail.com>
2023-09-04 13:13:36 -0300

    feat: Se agrega feign y cliente a api-products

---
+++ src/main/java/com/mlt/api/apioperations/client/apiproducts/domain/dto/data/ImageLinkDTO.java
@@ -0,0 +1,27 @@
+package com.mlt.api.apioperations.client.apiproducts.domain.dto.data;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mlt.api.common.domain.request.MltRequest;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ImageLinkDTO extends MltRequest {
+
+    @NotEmpty(message = "Image link cannot be empty")
+    private String link;
+    @NotEmpty(message = "Image link cannot be empty")
+    private String provider;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Integer id;
+
+}
